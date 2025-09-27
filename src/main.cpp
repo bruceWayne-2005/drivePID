e8a9df986e06a77001c0bdd9caadbfeaa9f39c35
bruceWayne-2005 <diveshkumar_23ee078@dtu.ac.in>
2025-09-27 23:56:32 +0530

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,6 @@
 #include <Arduino.h>
 #include <PID_v1.h>
 
-/*
-uint8_t encaPins[6]={PC9, PB8, PB9, PA5, PA6, PA7};
-uint8_t encbPins[6]={PC8, PC6, PC5, PA12, PA11, PB12};
-
-double pos[6], prevT[6];
-double Input[6], Output[6], Setpoint[6];
-double Kp = 2.0, Ki = 5.0, Kd = 1.0;
-
-PID motor0(&Input[0], &Output[0], &Setpoint[0], Kp, Ki, Kd, DIRECT);
-PID motor1(&Input[1], &Output[1], &Setpoint[1], Kp, Ki, Kd, DIRECT);
-PID motor2(&Input[2], &Output[2], &Setpoint[2], Kp, Ki, Kd, DIRECT);
-PID motor3(&Input[3], &Output[3], &Setpoint[3], Kp, Ki, Kd, DIRECT);
-PID motor4(&Input[4], &Output[4], &Setpoint[4], Kp, Ki, Kd, DIRECT);
-PID motor5(&Input[5], &Output[5], &Setpoint[5], Kp, Ki, Kd, DIRECT);
-
-
-void readEncoder(int i) {
-    int b = digitalRead(encbPins[i]);
-    int increment = (b > 0) ? 1 : -1;
-    Serial.println("debigging");
-    pos[i] += increment;
-
-    unsigned long currT = micros();
-    float deltaT = (currT - prevT[i]) / 1.0e6; // sec
-    if (deltaT > 0){
-        Input[i] = increment / deltaT;  // ticks/sec
-    }
-    prevT[i] = currT;
-}
-
-void readEncoder0(){readEncoder(0);}
-void readEncoder1(){readEncoder(1);}
-void readEncoder2(){readEncoder(2);}
-void readEncoder3(){readEncoder(3);}
-void readEncoder4(){readEncoder(4);}
-void readEncoder5(){readEncoder(5);}
-
-void setup() {
-    Serial.begin(115200);
-
-    for (int i = 0; i<6; i++){
-        pinMode(encaPins[i], INPUT);
-        pinMode(encbPins[i], INPUT);
-
-        pos[i] = 0;
-        prevT[i] = micros();
-        Input[i] = 0;
-        Output[i] = 0;
-        Setpoint[i] = 0;
-    }
-    
-
-    attachInterrupt(digitalPinToInterrupt(encaPins[0]), readEncoder0, RISING);
-    attachInterrupt(digitalPinToInterrupt(encaPins[1]), readEncoder1, RISING);
-    attachInterrupt(digitalPinToInterrupt(encaPins[2]), readEncoder2, RISING);
-    attachInterrupt(digitalPinToInterrupt(encaPins[3]), readEncoder3, RISING);
-    attachInterrupt(digitalPinToInterrupt(encaPins[4]), readEncoder4, RISING);
-    attachInterrupt(digitalPinToInterrupt(encaPins[5]), readEncoder5, RISING);
-
-    // PID Instances Setup
-    motor0.SetMode(AUTOMATIC);
-    motor1.SetMode(AUTOMATIC);
-    motor2.SetMode(AUTOMATIC);
-    motor3.SetMode(AUTOMATIC);
-    motor4.SetMode(AUTOMATIC);
-    motor5.SetMode(AUTOMATIC);
-    
-    // Output
-    motor0.SetOutputLimits(-255, 255);
-    motor1.SetOutputLimits(-255, 255);
-    motor2.SetOutputLimits(-255, 255);
-    motor3.SetOutputLimits(-255, 255);
-    motor4.SetOutputLimits(-255, 255);
-    motor5.SetOutputLimits(-255, 255);
-}
-
-void loop() {
-    Serial.println(pos[0]);
-
-    motor0.Compute();
-    motor1.Compute();
-    motor2.Compute();
-    motor3.Compute();
-    motor4.Compute();
-    motor5.Compute();
-
-    delay(1000);
-}
-*/
 
 // Motor Ctrl pin definitions
 uint32_t dirPins[6] = {PC10, PC12, PC15, PH0, PC2, PC3};
@@ -102,7 +13,7 @@ void setMotor(int i, int pwm){
     }
     else{
         digitalWrite(dirPins[i], HIGH);
-        analogWrite(pwmPins[i], pwm);
+        analogWrite(pwmPins[i], -pwm);
     }
 }
 
@@ -155,8 +66,8 @@ void setup(){
     for (int i = 0; i < 6; i++){
         pinMode(dirPins[i], OUTPUT);
         pinMode(pwmPins[i], OUTPUT);
-        pinMode(encaPins[i], INPUT);
-        pinMode(encbPins[i], INPUT);
+        pinMode(encaPins[i], INPUT_PULLUP);
+        pinMode(encbPins[i], INPUT_PULLUP);
 
         setTps[i] = 500;
     }
@@ -198,10 +109,11 @@ void loop(){
         }
         interrupts();
 
-        for (int i = 0; i < 6; i++){
-            tps[i] = ticks[i] / ((currT - prevT) / 1000.0); // ticks/sec
-        }
+        double dt = (currT - prevT) / 1000.0;
         prevT = currT;
+        for (int i = 0; i < 6; i++){
+            tps[i] = ticks[i] / dt; // ticks/sec
+        }
     
 
         motor0.Compute();
@@ -224,12 +136,12 @@ void loop(){
 
 
         for (int i = 0; i < 6; i++){
-            //if (pwm[i] > 50 || pwm[i] < -50){
+            if (pwm[i] > 50 || pwm[i] < -50){
                 setMotor(i, pwm[i]);
-            //}
-            //else{
-            //    setMotor(i, 0);
-            //}
+            }
+            else{
+                setMotor(i, 0);
+            }
         }
         
         Serial.println();
